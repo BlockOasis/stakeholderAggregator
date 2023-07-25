f0f0baa84a9207879517e06b9591f59d3e8ecf16
Aryamanraj <20cs3011@rgipt.ac.in>
2023-07-25 06:00:35 +0530

    all functions and files handler

---
+++ src/index.js
@@ -0,0 +1,29 @@
+const mqttClient = require('./mqtt/mqttClient');
+const dataHandler = require('./data/dataHandler');
+const config = require('../config');
+
+async function main() {
+  try {
+    // Connect to MQTT broker
+    await mqttClient.connect(config.mqtt);
+
+    // Handle incoming MQTT messages
+    mqttClient.onMessage(dataHandler.handleIncomingMessage);
+
+    // Gracefully close the connection on SIGINT (Ctrl+C)
+    process.on('SIGINT', async () => {
+      console.log('Closing the MQTT connection...');
+      await mqttClient.disconnect();
+      process.exit();
+    });
+  } catch (error) {
+    console.error('Error:', error);
+    process.exit(1);
+  }
+}
+
+// Call the main function to start the MQTT client
+main();
+
+// Export the main function so that it can be called from other modules if needed
+module.exports = main;
