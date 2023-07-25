e9533315f9896b3617253263ca596c86ae8b988c
Aryamanraj <20cs3011@rgipt.ac.in>
2023-07-25 18:20:49 +0530

    configuration files

--- config.js
+++ config.js
@@ -1,6 +1,8 @@
-module.exports = {
-    maxFileSizeBytes: 1000, // 1 KB (adjust as needed)
-    mqtt: require('./src/mqtt/mqttConfig'),
-    apiKey: 'd86662ba.e4523bf0f55e48fe8369fbdebb7293fd',
-  };
+  const fs = require('fs');
+  const path = require('path');
+  
+  const configPath = path.resolve(__dirname, './config.json');
+  const configData = JSON.parse(fs.readFileSync(configPath, 'utf8'));
+  
+  module.exports = configData;
   
