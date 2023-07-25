205336e67c808d69d4fd2922e14bb1373a08d0a3
Aryamanraj <20cs3011@rgipt.ac.in>
2023-07-25 05:55:17 +0530

    Configurations and logfiles

---
+++ config.js
@@ -0,0 +1,6 @@
+module.exports = {
+    maxFileSizeBytes: 1000, // 1 KB (adjust as needed)
+    mqtt: require('./src/mqtt/mqttConfig'),
+    apiKey: 'd86662ba.e4523bf0f55e48fe8369fbdebb7293fd',
+  };
+  
