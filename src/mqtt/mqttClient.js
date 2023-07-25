236cd03cda5caf22c9949e2422e2cf1b2fbc804a
Aryamanraj <20cs3011@rgipt.ac.in>
2023-07-25 05:57:08 +0530

    mqtt client to listen to publised data and its configuration file

---
+++ src/mqtt/mqttClient.js
@@ -0,0 +1,55 @@
+// src/mqtt/mqttclient.js
+const mqtt = require('mqtt');
+const mqttConfig = require('./mqttConfig');
+
+let client;
+
+async function connect(mqttConfig) {
+  return new Promise((resolve, reject) => {
+    client = mqtt.connect(`mqtt://${mqttConfig.brokerIp}:${mqttConfig.port}`);
+    client.on('connect', () => {
+      console.log('Connected to MQTT broker');
+      client.subscribe(mqttConfig.topic, (err) => {
+        if (err) {
+          console.error(`Error subscribing to topic: ${err}`);
+          reject(err);
+        } else {
+          resolve();
+        }
+      });
+    });
+
+    client.on('error', (err) => {
+      console.error('MQTT error:', err);
+      reject(err);
+    });
+
+    client.on('close', () => {
+      console.log('Disconnected from MQTT broker');
+    });
+  });
+}
+
+function onMessage(messageHandler) {
+  client.on('message', (topic, message) => {
+    messageHandler(topic, message.toString());
+  });
+}
+
+async function disconnect() {
+  return new Promise((resolve, reject) => {
+    if (client) {
+      client.end(true, () => {
+        resolve();
+      });
+    } else {
+      reject(new Error('MQTT client is not connected'));
+    }
+  });
+}
+
+module.exports = {
+  connect,
+  onMessage,
+  disconnect,
+};
---
+++ src/mqtt/mqttConfig.js
@@ -0,0 +1,6 @@
+module.exports = {
+    brokerIp: '192.168.1.5', // Replace with your MQTT broker IP
+    port: 1883, // Default MQTT port
+    topic: 'test', // Replace with your desired MQTT topic
+  };
+  
